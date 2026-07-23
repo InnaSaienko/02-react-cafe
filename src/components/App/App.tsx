e59f94674eb466e83ef9fd68d4250bf7f114d040
Inna Saienko <inna.saienko.nl@gmail.com>
2026-07-23 10:12:44 +0200

    feat(App): add vote functionality

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,7 +1,9 @@
 import css from './App.module.css'
 import {useState} from "react";
-import type {Votes} from "../../types/votes.ts";
+import type {Votes, VoteType} from "../../types/votes.ts";
 import CafeInfo from "../CafeInfo/CafeInfo.tsx";
+import VoteOptions from "../VoteOptions/VoteOptions.tsx";
+import VoteStats from "../VoteStats/VoteStats.tsx";
 
 const DEFAULT_VOTES = {
   good: 0,
@@ -9,14 +11,28 @@ const DEFAULT_VOTES = {
   bad: 0,
 }
 
+
+
 function App() {
   const [votes, setVotes] = useState<Votes>(DEFAULT_VOTES)
   const totalVotes = votes.good + votes.neutral + votes.bad;
+  const positiveRate = totalVotes
+        ? Math.round((votes.good / totalVotes) * 100)
+        : 0;
+
+    const handleVote = (vote: VoteType) => {
+        setVotes(prev => ({
+            ...prev,
+            [vote]: prev[vote] + 1,
+        }));
+    };
 
   return (
     <div className={css.app}>
       {totalVotes}
       <CafeInfo />
+      <VoteOptions onVote={handleVote} onReset={() => setVotes(DEFAULT_VOTES)} />
+      <VoteStats votes={votes} totalVotes={totalVotes} positiveRate={positiveRate}/>
     </div>
   )
 }
