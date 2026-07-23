e374b8915b464ab863b663e432c6c7e7989160a8
Inna Saienko <inna.saienko.nl@gmail.com>
2026-07-23 08:50:47 +0200

    feat(VoteOptions): create template

--- src/components/VoteOptions/VoteOptions.tsx
+++ src/components/VoteOptions/VoteOptions.tsx
@@ -1,3 +1,17 @@
-const VoteOptions = {}
+import css from "./VoteOptions.module.css";
+import type {VoteOptionsProps} from "../../types/votes.ts";
+import type {ReactElement} from "react";
+
+
+const VoteOptions = ({onVote, onReset}: VoteOptionsProps): ReactElement => {
+    return (
+        <div className={css.container}>
+            <button className={css.button} onClick={() => onVote("good")}>Good</button>
+            <button className={css.button} onClick={() => onVote("neutral")}>Neutral</button>
+            <button className={css.button} onClick={() => onVote("bad")}>Bad</button>
+            <button className={css.button} onClick={onReset}>Reset</button>
+        </div>
+    )
+}
 
 export default VoteOptions;
--- src/types/votes.ts
+++ src/types/votes.ts
@@ -1,3 +1,8 @@
 export type VoteType = "good" | "neutral" | "bad";
 
 export type Votes = Record<VoteType, number>;
+
+export interface VoteOptionsProps {
+    onVote: (vote: "good" | "neutral" | "bad") => void;
+    onReset: () => void;
+};
