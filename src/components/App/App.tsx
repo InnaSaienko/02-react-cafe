7be20620f883d57b5f7933579c3ddbbdc0907533
Inna Saienko <inna.saienko.nl@gmail.com>
2026-07-25 14:25:11 +0200

    fix: update "types/jest, vitejs, " so they're compatible

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -6,17 +6,16 @@ import VoteOptions from "../VoteOptions/VoteOptions.tsx";
 import VoteStats from "../VoteStats/VoteStats.tsx";
 
 const DEFAULT_VOTES = {
-  good: 0,
-  neutral: 0,
-  bad: 0,
+    good: 0,
+    neutral: 0,
+    bad: 0,
 }
 
 
-
 function App() {
-  const [votes, setVotes] = useState<Votes>(DEFAULT_VOTES)
-  const totalVotes = votes.good + votes.neutral + votes.bad;
-  const positiveRate = totalVotes
+    const [votes, setVotes] = useState<Votes>(DEFAULT_VOTES)
+    const totalVotes = votes.good + votes.neutral + votes.bad;
+    const positiveRate = totalVotes
         ? Math.round((votes.good / totalVotes) * 100)
         : 0;
 
@@ -27,14 +26,14 @@ function App() {
         }));
     };
 
-  return (
-    <div className={css.app}>
-      {totalVotes}
-      <CafeInfo />
-      <VoteOptions onVote={handleVote} onReset={() => setVotes(DEFAULT_VOTES)} />
-      <VoteStats votes={votes} totalVotes={totalVotes} positiveRate={positiveRate}/>
-    </div>
-  )
+    return (
+        <div className={css.app}>
+            {totalVotes}
+            <CafeInfo/>
+            <VoteOptions onVote={handleVote} onReset={() => setVotes(DEFAULT_VOTES)}/>
+            <VoteStats votes={votes} totalVotes={totalVotes} positiveRate={positiveRate}/>
+        </div>
+    )
 }
 
 export default App
