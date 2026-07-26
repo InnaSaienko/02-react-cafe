ff88efefd18dc1f6f082e7fc846fe9c20739ad1f
Inna Saienko <inna.saienko.nl@gmail.com>
2026-07-26 07:26:46 +0200

    fix(Notification): implement component with required message

--- src/components/Notification/Notification.tsx
+++ src/components/Notification/Notification.tsx
@@ -1,3 +1,7 @@
-const Notification = () => {}
+import css from "./Notification.module.css";
+
+const Notification = () => {
+    return <p className={css.message}>No feedback yet</p>;
+}
 
 export default Notification;
